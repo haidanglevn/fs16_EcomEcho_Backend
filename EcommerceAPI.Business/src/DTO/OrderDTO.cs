f66ebcde466b54735ecd5d4435be87135af148a3
Dang Le <haidang.levn@gmail.com>
2024-01-01 22:50:49 +0200

    Slight edit to OrderDTO

--- EcommerceAPI.Business/src/DTO/OrderDTO.cs
+++ EcommerceAPI.Business/src/DTO/OrderDTO.cs
@@ -7,6 +7,7 @@ namespace EcommerceAPI.Business.src.DTO
         public Guid Id { get; set; }
         public Status Status { get; set; }
         public Guid UserId { get; set; }
+        public Guid AddressId { get; set; }
         public ICollection<OrderItemReadDTO> OrderItems { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
