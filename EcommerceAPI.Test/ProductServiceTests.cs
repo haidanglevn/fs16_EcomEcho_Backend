d843191842f3d18bcb03e6b1eb13f03769b49a6e
Dang Le <haidang.levn@gmail.com>
2024-01-02 04:53:08 +0200

    Add basic tests for ProductService

---
+++ EcommerceAPI.Test/ProductServiceTests.cs
@@ -0,0 +1,90 @@
+using AutoMapper;
+using EcommerceAPI.Business.src.Abstraction;
+using EcommerceAPI.Business.src.DTO;
+using EcommerceAPI.Business.src.Service;
+using EcommerceAPI.Business.src.Shared;
+using EcommerceAPI.Core.src.Abstraction;
+using EcommerceAPI.Core.src.Entity;
+using EcommerceAPI.Core.src.Parameter;
+using FluentAssertions;
+using Moq;
+
+namespace EcommerceAPI.Test
+{
+    public class ProductServiceTests
+    {
+        private readonly ProductService _productService;
+        private readonly Mock<IProductRepo> _mockProductRepo;
+        private readonly Mock<ICategoryRepo> _mockCategoryRepo;
+        private IMapper _mapper;
+        public static readonly Guid FashionProduct1Guid = Guid.Parse("bc2983a5-b13c-49a4-9a6c-29c4dad9c355");
+        public static readonly Guid FashionProduct2Guid = Guid.Parse("fedfd1c9-a73a-47bf-a635-370c193fc1df");
+        public static readonly Guid Category1Guid = Guid.Parse("88499694-269b-4308-a64d-0e903a1f503d");
+        public GetAllParams options;
+        public Product mockProduct1 = new()
+        {
+            Id = FashionProduct1Guid,
+            Title = "Fashion Product 1",
+            Description = "Description for Fashion Product 1",
+            Price = 100,
+            CategoryId = Category1Guid,
+        };
+        public Product mockProduct2 = new()
+        {
+            Id = FashionProduct2Guid,
+            Title = "Fashion Product 2",
+            Description = "Description for Fashion Product 2",
+            Price = 140,
+            CategoryId = Category1Guid,
+        };
+
+        public List<Product> mockProducts;
+
+        public ProductServiceTests()
+        {
+            options = new GetAllParams { Limit = 10, Offset = 0, Search = "" };
+            _mockProductRepo = new Mock<IProductRepo>();
+            _mockCategoryRepo = new Mock<ICategoryRepo>();
+            var mapperProfile = new MapperProfile();
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(mapperProfile));
+            _mapper = new Mapper(config);
+
+            mockProducts = new()
+            {
+                mockProduct1, mockProduct2
+            };
+
+
+            _mockProductRepo.Setup(repo => repo.DeleteProduct(FashionProduct2Guid)).Returns(true);
+            _mockProductRepo.Setup(repo => repo.GetAllProducts(options)).Returns(mockProducts);
+            _mockProductRepo.Setup(repo => repo.GetOneProduct(FashionProduct1Guid)).Returns(mockProduct1);
+            _mockProductRepo.Setup(repo => repo.CheckProductExist(FashionProduct1Guid)).Returns(true);
+            _productService = new ProductService(_mockProductRepo.Object, _mapper, _mockCategoryRepo.Object);
+        }
+
+        [Fact]
+        public void ProductService_DeleteProduct_ShouldDeleteRightProduct()
+        {
+            var result = _productService.DeleteProduct(FashionProduct2Guid);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ProductService_GetAllProducts_ReturnsAllProducts()
+        {
+            var result = _productService.GetAllProducts(options);
+            result.Should().NotBeEmpty().And.HaveCount(2);
+        }
+
+        [Fact]
+        public void ProductService_GetOneProduct_ReturnsTheRightProduct()
+        {
+            var result = _productService.GetOneProduct(FashionProduct1Guid);
+
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(ProductReadDTO));
+        }
+
+    }
+}
